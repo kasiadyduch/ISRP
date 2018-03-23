31d37ebd5ec6d665e37ee6ccb2d60f66938826d9
charl0tte <karolinakozak9@gmail.com>
2018-03-23 16:08:35 +0100

    Poprawki po obronie

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by Karolina on 30.11.2017.
  */
 
-//Klasa do odpalania REST-a!
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
